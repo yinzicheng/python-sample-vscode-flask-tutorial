2dbb569026d117ee8a624ecd14bf7733fd60be25
zicheng yin <yinzicheng@gmail.com>
2023-01-03 17:40:05 +1100

    update

--- hello_app/webapp.py
+++ hello_app/webapp.py
@@ -4,3 +4,4 @@ from . import views  # For import side-effects of setting up routes.
 
 # Time-saver: output a URL to the VS Code terminal so you can easily Ctrl+click to open a browser
 # print('http://127.0.0.1:5000/hello/VSCode')
+# flask --app webapp run
